f8d5020937c993c38e6605ecea0c88b3aa710cca
Artyom Ganev <a.a.ganev@gmail.com>
2019-06-30 17:29:02 +0300

    https://ru.hexlet.io/courses/js-functions/lessons/guard-expression/exercise_unit

--- JavaScript/Ex/_functions/ex1/isPalindrome.ts
+++ JavaScript/Ex/_functions/ex1/isPalindrome.ts
@@ -1,3 +1,19 @@
-export default (string) => {
-   return true;
+const MIN_LENGTH = 2;
+
+/**
+ * Check if string is palindrome
+ * @param {String} str
+ * @return {Boolean} check result
+ */
+const isPalindrome = (str) => {
+    const length = str.length;
+    if (length < MIN_LENGTH) {
+        return true;
+    }
+    const end = length - 1;
+    if (str[0] !== str[end]) {
+        return false;
+    }
+    return isPalindrome(str.slice(1, end));
 };
+export default isPalindrome;
